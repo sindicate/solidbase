7370e6b108e9c839c50972fd4dcae28158403c0a
rene.de.bloois@gmail.com <rene.de.bloois@gmail.com@b7c04ee0-c715-11de-a2e8-8d36a3108c74>
2009-11-07 12:17:24 +0000

    Ant messed up special characters like the é in my name. Need to fix that later.

--- trunk/test/src/solidbase/test/ant/UpgradeTaskTests.java
+++ trunk/test/src/solidbase/test/ant/UpgradeTaskTests.java
@@ -14,7 +14,7 @@ public class UpgradeTaskTests extends BuildFileTest
 		configureProject( "test-upgradetask.xml" );
 		executeTarget( "ant-test" );
 		String log = TestUtil.generalizeOutput( getLog() );
-		Assert.assertEquals( log, "SolidBase v1.5.x(C) 2006-2009 René M. de BlooisConnecting to database...The database has no version yet.Opening patchfile 'file:/.../testpatch-multiconnections.sql'    Encoding is 'ISO-8859-1'Patching to \"1.0.1\"    Creating table DBVERSION.    Creating table DBVERSIONLOG.Patching \"1.0.1\" to \"1.0.2\"    Creating table USERS.    Inserting admin user.The database has been patched.Current database version is \"1.0.2\"." );
+		Assert.assertEquals( log, "SolidBase v1.5.x(C) 2006-2009 Rene M. de BlooisConnecting to database...The database has no version yet.Opening patchfile 'file:/.../testpatch-multiconnections.sql'    Encoding is 'ISO-8859-1'Patching to \"1.0.1\"    Creating table DBVERSION.    Creating table DBVERSIONLOG.Patching \"1.0.1\" to \"1.0.2\"    Creating table USERS.    Inserting admin user.The database has been patched.Current database version is \"1.0.2\"." );
 	}
 
 	@Test(groups={"new"})
@@ -23,7 +23,7 @@ public class UpgradeTaskTests extends BuildFileTest
 		configureProject( "test-upgradetask.xml" );
 		executeTarget( "ant-basedir-test" );
 		String log = TestUtil.generalizeOutput( getLog() );
-		Assert.assertEquals( log, "SolidBase v1.5.x(C) 2006-2009 René M. de BlooisConnecting to database...The database has no version yet.Opening patchfile 'C:\\...\\testpatch-basedir.sql'    Encoding is 'ISO-8859-1'Patching to \"1.0.1\"    Creating table DBVERSION.    Creating table DBVERSIONLOG.Patching \"1.0.1\" to \"1.0.2\"    Creating table USERS.    Inserting admin user.The database has been patched.Current database version is \"1.0.2\"." );
+		Assert.assertEquals( log, "SolidBase v1.5.x(C) 2006-2009 Rene M. de BlooisConnecting to database...The database has no version yet.Opening patchfile 'C:\\...\\testpatch-basedir.sql'    Encoding is 'ISO-8859-1'Patching to \"1.0.1\"    Creating table DBVERSION.    Creating table DBVERSIONLOG.Patching \"1.0.1\" to \"1.0.2\"    Creating table USERS.    Inserting admin user.The database has been patched.Current database version is \"1.0.2\"." );
 	}
 }
 
