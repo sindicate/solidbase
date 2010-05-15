2b2f84e2d761934314f38ccafd82aba9039d99bd
rene.de.bloois@gmail.com <rene.de.bloois@gmail.com@b7c04ee0-c715-11de-a2e8-8d36a3108c74>
2010-05-15 10:42:47 +0000

    Introduced the CommandFileException, which is a FatalException plus linenumber. Fixed a warning from the Ant javac task: "warning: 'includeantruntime' was not set, defaulting to build.sysclasspath=last; set to false for repeatable builds".

---
+++ trunk/src/solidbase/core/CommandFileException.java
@@ -0,0 +1,49 @@
+/*--
+ * Copyright 2010 René M. de Bloois
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package solidbase.core;
+
+/**
+ * Represents a problem in the command file (upgrade file or SQL file). A stack trace is not required. The failure and
+ * line number are enough to identify corrective actions.
+ * 
+ * @author René M. de Bloois
+ */
+public class CommandFileException extends FatalException
+{
+	/**
+	 * The line number in the command file where the problem is located.
+	 */
+	protected int lineNumber;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param message The failure message.
+	 * @param lineNumber The line number in the command file where the problem is located.
+	 */
+	public CommandFileException( String message, int lineNumber )
+	{
+		super( message );
+		this.lineNumber = lineNumber;
+	}
+
+	@Override
+	public String getMessage()
+	{
+		return super.getMessage() + ", at line " + this.lineNumber;
+	}
+}
