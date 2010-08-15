753c532f02870e942998ca0149885e9fb43e39d9
rene.de.bloois@gmail.com <rene.de.bloois@gmail.com>
2010-08-15 19:07:37 +0000

    Updated the example upgrade script according to release 1.6.1.

--- dist/upgrade-hsqldb-example.sql
+++ dist/upgrade-hsqldb-example.sql
@@ -19,14 +19,14 @@
 
 
 --*	DEFINITION
---*		INIT "" --> "1.1"
+--*		SETUP "" --> "1.1"
 --*		UPGRADE "" --> "1.0.1"
 --*		UPGRADE "1.0.1" --> "1.0.2"
 --*	/DEFINITION
 
 
 
---* INIT "" --> "1.1"
+--* SETUP "" --> "1.1"
 
 --* SECTION "Creating SolidBase control tables"
 CREATE TABLE DBVERSION
@@ -48,7 +48,7 @@ CREATE TABLE DBVERSIONLOG
 );
 CREATE INDEX DBVERSIONLOG_INDEX1 ON DBVERSIONLOG ( TYPE, TARGET );
 
---* /INIT
+--* /SETUP
 
 
 
