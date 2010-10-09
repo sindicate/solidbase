d734b7ca76b8cb817202dca9acfbecb324315e82
rene.de.bloois@gmail.com <rene.de.bloois@gmail.com@b7c04ee0-c715-11de-a2e8-8d36a3108c74>
2010-10-09 18:37:36 +0000

    Added SIGTERM/CTRL-C protection to the PatchProcessor. CTRL-C will now interrupt the main thread instead of aborting it.

---
+++ trunk/src/solidbase/core/ShutdownHook.java
@@ -0,0 +1,60 @@
+/*--
+ * Copyright 2010 René M. de Bloois
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package solidbase.core;
+
+/**
+ * This shutdown hook prevents ctrl-c from aborting all threads.
+ * Instead it will call {@link Thread#interrupt()} on the main thread.
+ * 
+ * @author René M. de Bloois
+ */
+public class ShutdownHook extends Thread
+{
+	/**
+	 * The main thread.
+	 */
+	protected Thread main;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param main The main thread.
+	 */
+	public ShutdownHook( Thread main )
+	{
+		this.main = main;
+	}
+
+	/**
+	 * This method gets triggered when the JVM wants to exit.
+	 * 
+	 * @see Runtime#addShutdownHook(Thread)
+	 */
+	@Override
+	public void run()
+	{
+		try
+		{
+			this.main.interrupt();
+			this.main.join(); // Endless wait because of deadlock
+		}
+		catch( InterruptedException e )
+		{
+			// OK to stop here
+		}
+	}
+}
