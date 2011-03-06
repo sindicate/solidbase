bc90f0f8e71d62227e82c95cdce9071962c74917
rene.de.bloois@gmail.com <rene.de.bloois@gmail.com@b7c04ee0-c715-11de-a2e8-8d36a3108c74>
2011-03-06 21:12:01 +0000

    Added copyrights. Add package.html to the spring package.

--- branches/changes/spring-bean/src/solidbase/util/WorkerThread.java
+++ branches/changes/spring-bean/src/solidbase/util/WorkerThread.java
@@ -1,8 +1,24 @@
+/*--
+ * Copyright 2010 René M. de Bloois
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package solidbase.util;
 
 /**
  * Worker thread which stores the exception in case it ended with one. It also stores if the thread ended itself by throwing a ThreadDeath error.
- * 
+ *
  * @author René M. de Bloois
  */
 abstract public class WorkerThread extends Thread
@@ -61,7 +77,7 @@ abstract public class WorkerThread extends Thread
 
 	/**
 	 * Returns the RuntimeException that the thread has thrown.
-	 * 
+	 *
 	 * @return the RuntimeException that the thread has thrown. Null if no exception has been thrown.
 	 */
 	public RuntimeException getException()
@@ -71,7 +87,7 @@ abstract public class WorkerThread extends Thread
 
 	/**
 	 * Returns true if the thread threw a {@link ThreadDeath} error to indicated that it was interrupted, false otherwise.
-	 * 
+	 *
 	 * @return true if the thread threw a {@link ThreadDeath} error to indicated that it was interrupted, false otherwise.
 	 */
 	public boolean isThreadDeath()
