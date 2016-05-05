38ef7926fac90323c77497dda071bd527ed39cbf
René <rene.de.bloois@gmail.com>
2016-05-05 20:32:06 +0200

    Working on CBOR file format export and import.

---
+++ src/solidbase/core/plugins/CBORDataWriter.java
@@ -0,0 +1,105 @@
+/*--
+ * Copyright 2015 René M. de Bloois
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package solidbase.core.plugins;
+
+import java.io.OutputStream;
+import java.sql.Blob;
+import java.sql.Clob;
+import java.sql.SQLException;
+import java.util.ListIterator;
+import java.util.Map;
+
+import solidstack.cbor.CBOROutputStream;
+import solidstack.io.Resource;
+import solidstack.io.SourceLocation;
+import solidstack.json.JSONArray;
+
+public class CBORDataWriter implements DataProcessor
+{
+	private Resource resource;
+	private CBOROutputStream cborOutputStream;
+	private FileSpec[] fileSpecs;
+	private Column[] columns;
+	private SourceLocation location;
+	private Map<String, ColumnSpec> columnSpecs;
+
+	public CBORDataWriter( Resource resource, OutputStream out, Map<String, ColumnSpec> columnSpecs, SourceLocation location )
+	{
+		this.resource = resource;
+		this.cborOutputStream = new CBOROutputStream( out );
+		this.columnSpecs = columnSpecs;
+		this.location = location;
+	}
+
+	public void init( Column[] columns )
+	{
+		this.columns = columns;
+
+		if( this.columnSpecs != null )
+		{
+			this.fileSpecs = new FileSpec[ columns.length ];
+			for( int i = 0; i < columns.length; i++ )
+				{
+					ColumnSpec columnSpec = this.columnSpecs.get( columns[ i ].getName() );
+					if( columnSpec != null )
+						this.fileSpecs[ i ] = columnSpec.toFile;
+				}
+		}
+	}
+
+	public void process( Object[] values ) throws SQLException
+	{
+		int columns = values.length;
+
+		JSONArray array = new JSONArray();
+		for( int i = 0; i < columns; i++ )
+		{
+			Object value = values[ i ];
+			if( value == null )
+			{
+				array.add( null );
+				continue;
+			}
+
+			if( value instanceof Clob )
+				array.add( ( (Clob)value ).getCharacterStream() ); // TODO Need to close these?
+			else if( value instanceof Blob )
+				array.add( ( (Blob)value ).getBinaryStream() );
+			else
+				array.add( value );
+		}
+
+		for( ListIterator< Object > i = array.iterator(); i.hasNext(); )
+		{
+			Object value = i.next();
+			if( value instanceof java.sql.Date || value instanceof java.sql.Time || value instanceof java.sql.Timestamp || value instanceof java.sql.RowId )
+				i.set( value.toString() );
+		}
+
+		this.cborOutputStream.write( array );
+	}
+
+	public void close()
+	{
+		this.cborOutputStream.close();
+	}
+
+//	public JSONWriter getJSONWriter()
+//	{
+//		return this.jsonWriter;
+//	}
+}
---
+++ src/solidbase/core/plugins/DumpCBOR.java
@@ -0,0 +1,379 @@
+/*--
+ * Copyright 2011 René M. de Bloois
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package solidbase.core.plugins;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.regex.Pattern;
+import java.util.zip.GZIPOutputStream;
+
+import solidbase.core.Command;
+import solidbase.core.CommandListener;
+import solidbase.core.CommandProcessor;
+import solidbase.core.SourceException;
+import solidbase.util.FixedIntervalLogCounter;
+import solidbase.util.LogCounter;
+import solidbase.util.SQLTokenizer;
+import solidbase.util.SQLTokenizer.Token;
+import solidbase.util.TimeIntervalLogCounter;
+import solidstack.io.FatalIOException;
+import solidstack.io.FileResource;
+import solidstack.io.Resource;
+import solidstack.io.SourceReaders;
+import solidstack.script.scopes.AbstractScope;
+
+
+/**
+ * This plugin executes EXPORT CSV statements.
+ *
+ * @author René M. de Bloois
+ * @since Aug 12, 2011
+ */
+public class DumpCBOR implements CommandListener
+{
+	static private final Pattern triggerPattern = Pattern.compile( "\\s*DUMP\\s+CBOR\\s+.*", Pattern.DOTALL | Pattern.CASE_INSENSITIVE );
+
+
+	//@Override
+	// TODO Escape dynamic file names, because illegal characters may be generated
+	// TODO Export multiple tables to a single file. If no PK than sort on all columns. Schema name for import or not?
+	// TODO COLUMN TO TEXT FILE with ENCODING
+	public boolean execute( CommandProcessor processor, Command command, boolean skip ) throws SQLException
+	{
+		if( !triggerPattern.matcher( command.getCommand() ).matches() )
+			return false;
+
+		if( skip )
+			return true;
+
+		Parsed parsed = parse( command );
+
+		AbstractScope scope = processor.getContext().getScope(); // TODO Scope?
+
+		Resource cborOutput = new FileResource( new File( parsed.fileName ) ); // Relative to current folder
+
+		try
+		{
+			OutputStream out = cborOutput.getOutputStream();
+			if( parsed.gzip )
+				out = new BufferedOutputStream( new GZIPOutputStream( out, 65536 ), 65536 );
+			try
+			{
+				Statement statement = processor.createStatement();
+				try
+				{
+					ResultSet result = statement.executeQuery( parsed.query );
+
+					LogCounter counter = null;
+					if( parsed.logRecords > 0 )
+						counter = new FixedIntervalLogCounter( parsed.logRecords );
+					else if( parsed.logSeconds > 0 )
+						counter = new TimeIntervalLogCounter( parsed.logSeconds );
+
+					DBReader reader = new DBReader( result, counter != null ? new ExportLogger( counter, processor.getProgressListener() ) : null, parsed.dateAsTimestamp );
+					RecordSource source = reader;
+
+					Column[] columns = source.getColumns();
+					int count = columns.length;
+
+					FileSpec[] fileSpecs = new FileSpec[ count ];
+
+					// Analyze columns
+
+					SelectProcessor selector = new SelectProcessor();
+					if( parsed.columns != null )
+						for( Entry<String, ColumnSpec> entry : parsed.columns.entrySet() )
+							if( entry.getValue().skip )
+								selector.deselect( entry.getKey() );
+					for( Column column : columns )
+					{
+						int type = column.getType();
+						// TODO STRUCT serialize
+						// TODO This must be optional and not the default
+						if( type == 2002 || column.getTypeName() == null )
+							selector.deselect( column.getName() );
+					}
+
+					if( parsed.columns != null )
+						for( int i = 0; i < count; i++ )
+						{
+							ColumnSpec columnSpec = parsed.columns.get( columns[ i ].getName() );
+							if( columnSpec != null )
+								fileSpecs[ i ] = columnSpec.toFile;
+						}
+
+					// Connect FileSpecs with the DBReader for the original values
+					for( FileSpec fileSpec : fileSpecs )
+						if( fileSpec != null )
+							fileSpec.setSource( reader );
+
+					if( parsed.coalesce != null )
+					{
+						CoalescerProcessor coalescer = new CoalescerProcessor( parsed.coalesce );
+						source.setOutput( coalescer );
+						source = coalescer;
+					}
+
+					if( selector.hasDeselected() )
+					{
+						source.setOutput( selector );
+						source = selector;
+					}
+
+					CBORDataWriter dataWriter = new CBORDataWriter( cborOutput, out, parsed.columns, command.getLocation() );
+					try
+					{
+						source.setOutput( dataWriter );
+						reader.init();
+						columns = source.getColumns();
+
+						// TODO Write header
+
+						try
+						{
+							reader.process();
+						}
+						finally
+						{
+							// Close files that have been left open
+							for( FileSpec fileSpec : fileSpecs )
+								if( fileSpec != null )
+								{
+									if( fileSpec.out != null )
+										fileSpec.out.close();
+									if( fileSpec.writer != null )
+										fileSpec.writer.close();
+								}
+						}
+					}
+					finally
+					{
+						dataWriter.close();
+					}
+				}
+				finally
+				{
+					processor.closeStatement( statement, true );
+				}
+			}
+			finally
+			{
+				out.close();
+			}
+		}
+		catch( IOException e )
+		{
+			throw new FatalIOException( e );
+		}
+
+		return true;
+	}
+
+
+	/**
+	 * Parses the given command.
+	 *
+	 * @param command The command to be parsed.
+	 * @return A structure representing the parsed command.
+	 */
+	// TODO The order of stuff should be free
+	static protected Parsed parse( Command command )
+	{
+		/*
+		DUMP CBOR
+		DATE AS TIMESTAMP
+		COALESCE col1, col2
+		LOG EVERY n (RECORDS|SECONDS)
+		FILE "file" [GZIP]
+		COLUMN col1, col2 ( TO (BINARY|TEXT) FILE "file" [THRESHOLD n] | SKIP )
+		*/
+
+		Parsed result = new Parsed();
+
+		SQLTokenizer tokenizer = new SQLTokenizer( SourceReaders.forString( command.getCommand(), command.getLocation() ) );
+
+		tokenizer.get( "DUMP" );
+		tokenizer.get( "JSON" );
+
+		Token t = tokenizer.get( "DATE", "COALESCE", "LOG", "FILE" );
+
+		if( t.eq( "DATE" ) )
+		{
+			tokenizer.get( "AS" );
+			tokenizer.get( "TIMESTAMP" );
+
+			result.dateAsTimestamp = true;
+
+			t = tokenizer.get( "COALESCE", "LOG", "FILE" );
+		}
+
+		while( t.eq( "COALESCE" ) )
+		{
+			if( result.coalesce == null )
+				result.coalesce = new ArrayList<List<String>>();
+
+			List<String> cols = new ArrayList<String>();
+			result.coalesce.add( cols );
+
+			t = tokenizer.get();
+			if( t.isString() || t.isNewline() || t.isEndOfInput() || t.isNumber() )
+				throw new SourceException( "Expecting a column name, not [" + t + "]", tokenizer.getLocation() );
+			cols.add( t.toString() );
+
+			t = tokenizer.get( "," );
+			do
+			{
+				t = tokenizer.get();
+				if( t.isString() || t.isNewline() || t.isEndOfInput() || t.isNumber() )
+					throw new SourceException( "Expecting a column name, not [" + t + "]", tokenizer.getLocation() );
+				cols.add( t.toString() );
+
+				t = tokenizer.get();
+			}
+			while( t.eq( "," ) );
+		}
+
+		tokenizer.expect( t, "LOG", "FILE" );
+
+		if( t.eq( "LOG" ) )
+		{
+			tokenizer.get( "EVERY" );
+			t = tokenizer.get();
+			if( !t.isNumber() )
+				throw new SourceException( "Expecting a number, not [" + t + "]", tokenizer.getLocation() );
+
+			int interval = Integer.parseInt( t.getValue() );
+			t = tokenizer.get( "RECORDS", "SECONDS" );
+			if( t.eq( "RECORDS" ) )
+				result.logRecords = interval;
+			else
+				result.logSeconds = interval;
+
+			tokenizer.get( "FILE" );
+		}
+
+		t = tokenizer.get();
+		if( !t.isString() )
+			throw new SourceException( "Expecting filename enclosed in double quotes, not [" + t + "]", tokenizer.getLocation() );
+		result.fileName = t.stripQuotes();
+
+		t = tokenizer.get();
+		if( t.eq( "GZIP" ) )
+		{
+			result.gzip = true;
+			t = tokenizer.get();
+		}
+
+		while( t.eq( "COLUMN" ) )
+		{
+			if( result.columns == null )
+				result.columns = new HashMap<String, ColumnSpec>();
+
+			List<String> cols = new ArrayList<String>();
+			do
+			{
+				t = tokenizer.get();
+				if( t.isString() || t.isNewline() || t.isEndOfInput() || t.isNumber() )
+					throw new SourceException( "Expecting a column name, not [" + t + "]", tokenizer.getLocation() );
+				cols.add( t.getValue() );
+				t = tokenizer.get();
+			}
+			while( t.eq( "," ) );
+			tokenizer.push( t );
+
+			ColumnSpec columnSpec;
+			t = tokenizer.get( "TO", "SKIP" );
+			if( t.eq( "TO" ) )
+			{
+				t = tokenizer.get( "BINARY", "TEXT" );
+				boolean binary = t.eq( "BINARY" );
+				tokenizer.get( "FILE" );
+				t = tokenizer.get();
+				if( !t.isString() )
+					throw new SourceException( "Expecting filename enclosed in double quotes, not [" + t + "]", tokenizer.getLocation() );
+				String fileName = t.stripQuotes();
+
+				t = tokenizer.get();
+				int threshold = 0;
+				if( t.eq( "THRESHOLD" ) )
+				{
+					threshold = Integer.parseInt( tokenizer.get().getValue() );
+					t = tokenizer.get();
+				}
+
+				columnSpec = new ColumnSpec( false, new FileSpec( binary, fileName, threshold ) );
+			}
+			else
+			{
+				columnSpec = new ColumnSpec( true, null );
+				t = tokenizer.get();
+			}
+
+			for( String col : cols )
+				result.columns.put( col, columnSpec );
+		}
+
+		tokenizer.push( t );
+
+		result.query = tokenizer.getRemaining();
+
+		return result;
+	}
+
+
+	//@Override
+	public void terminate()
+	{
+		// Nothing to clean up
+	}
+
+
+	/**
+	 * A parsed command.
+	 *
+	 * @author René M. de Bloois
+	 */
+	static protected class Parsed
+	{
+		/** The file path to export to */
+		protected String fileName;
+		protected boolean gzip;
+
+		/** The query */
+		protected String query;
+
+		protected boolean dateAsTimestamp;
+
+		/** Which columns need to be coalesced */
+		protected List<List<String>> coalesce;
+
+		protected int logRecords;
+		protected int logSeconds;
+
+		protected Map<String, ColumnSpec> columns;
+	}
+}
