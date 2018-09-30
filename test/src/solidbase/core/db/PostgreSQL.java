6934d4d574de3a8219adc3f2237960f2ec3f05ff
René de Bloois <rene.de.bloois@gmail.com>
2018-09-30 14:03:51 +0200

    Upgraded jmockit.

--- test/src/solidbase/core/db/PostgreSQL.java
+++ test/src/solidbase/core/db/PostgreSQL.java
@@ -16,36 +16,51 @@
 
 package solidbase.core.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Set;
 
-import mockit.Mockit;
-
 import org.testng.annotations.Test;
 
+import mockit.Invocation;
+import mockit.Mock;
+import mockit.MockUp;
 import solidbase.core.Setup;
 import solidbase.core.TestUtil;
 import solidbase.core.UpgradeProcessor;
-import solidbase.test.mocks.PostgreSQLDriverManager;
+import solidbase.test.mocks.PostgreSQLConnection;
 
+
+@SuppressWarnings( "javadoc" )
 public class PostgreSQL
 {
-	@Test
-	public void testTransactionAborted() throws SQLException
-	{
-		TestUtil.dropHSQLDBSchema( Setup.defaultdb, "sa", null );
+	// See https://www.baeldung.com/jmockit-static-method
 
-		// This mock DriverManager (and mock Connection, PreparedStatement) simulate PostgreSQL
-		// PostgreSQL aborts a transaction when an SQLException is raised, so you can't continue with it
-		Mockit.setUpMocks( PostgreSQLDriverManager.class );
+	@SuppressWarnings( "unused" )
+	@Test
+	public void testTransactionAborted() throws SQLException {
+
+		// This mock DriverManager (and mock Connection, PreparedStatement) simulate PostgreSQL,
+		// PostgreSQL aborts a transaction when an SQLException is raised, so you can't continue with it.
+		new MockUp<DriverManager>() {
+			@Mock
+	    	public Connection getConnection( Invocation invocation, String url, String user, String password ) throws SQLException {
+	    		Connection result = invocation.proceed( url, user, password );
+	    		return new PostgreSQLConnection( result );
+	    	}
+	    };
+
+		TestUtil.dropHSQLDBSchema( Setup.defaultdb, "sa", null );
 
 		UpgradeProcessor patcher = Setup.setupUpgradeProcessor( "testpatch1.sql", Setup.defaultdb );
 
-		Set< String > targets = patcher.getTargets( false, null, false );
+		Set<String> targets = patcher.getTargets( false, null, false );
 		assert targets.size() > 0;
 		patcher.upgrade( "1.0.2" );
 		TestUtil.verifyVersion( patcher, "1.0.2", null, 2, null );
 
 		patcher.end();
 	}
+
 }
